22225a2a23572e5a9958fda8c6ee61208148f07c
Sergio GAMA <sergiogama@hotmail.com>
2020-07-29 12:45:43 -0300

    Add files via upload

---
+++ action-node.js
@@ -0,0 +1,15 @@
+/**
+  *
+  * main() will be run when you invoke this action
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+function main(params) {
+    if(params.tipoConta == "Conta")
+        return { saldo: 10000 }
+    else
+	    return { saldo: 50000 };
+}
